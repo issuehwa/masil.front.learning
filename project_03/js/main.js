18d122f593167a131cd974507a570e067806b6cd
SYililiii <witiss0@daum.net>
2022-12-29 15:46:35 +0900

    반복문 종류별로 추가

--- project_03/js/main.js
+++ project_03/js/main.js
@@ -10,6 +10,8 @@ let count = 1;
 
 function reander() {
   
+  // for 문
+
   for (let i = inputList.length-1; i >= 0; i--) {
     
     let item = inputList[i];
@@ -29,12 +31,102 @@ function reander() {
 
   // for in 문
 
+  // for (let i in inputList) {
+
+  //   let item = inputList[i];
+
+  //   let temp = `<ul id="ul_${count}">`
+  //   temp    += `  <li>${i++}</li>`
+  //   temp   += `  <li><a href="./detail.html?id=${item.id}">${item.title}</a></li>`
+  //   temp   += `  <li>${item.reg}</li>`
+  //   temp   += `  <li>${item.reg_date}</li>`
+  //   temp    += `</ul>`
+  //   temp    += `<hr />`
+
+  //   list.innerHTML += temp;
+
+  //   count++;
+  // }
+
   // for of 문
+
+  // for (const value of inputList) {
+
+  //   let temp = `<ul id="ul_${count}">`
+  //   temp    += `  <li>${count}</li>`
+  //   temp   += `  <li><a href="./detail.html?id=${value.id}">${value.title}</a></li>`
+  //   temp   += `  <li>${value.reg}</li>`
+  //   temp   += `  <li>${value.reg_date}</li>`
+  //   temp    += `</ul>`
+  //   temp    += `<hr />`
+
+  //   list.innerHTML += temp;
+
+  //   count++;
+  
+  // }
+
 
   // forEach 문
 
+  // inputList.forEach((value) => {
+
+  //   console.log(value)
+
+  //   let temp = `<ul id="ul_${count}">`
+  //   temp    += `  <li>${count}</li>`
+  //   temp   += `  <li><a href="./detail.html?id=${value.id}">${value.title}</a></li>`
+  //   temp   += `  <li>${value.reg}</li>`
+  //   temp   += `  <li>${value.reg_date}</li>`
+  //   temp    += `</ul>`
+  //   temp    += `<hr />`
+
+  //   list.innerHTML += temp;
+
+  //   count++;
+  // });
+
   // while 문
+
+  // let i=0;
+  
+  // while ( inputList.length >= i ) {
+    
+  //   let item = inputList[i];
+      
+  //   let temp = `<ul id="ul_${i}">`
+  //   temp    += `  <li>${i}</li>`
+  //   temp   += `  <li><a href="./detail.html?id=${item.id}">${item.title}</a></li>`
+  //   temp   += `  <li>${item.reg}</li>`
+  //   temp   += `  <li>${item.reg_date}</li>`
+  //   temp    += `</ul>`
+  //   temp    += `<hr />`
+
+  //   list.innerHTML += temp;
+
+  //   i++;
+  // }
  
   // do while 문
+
+  // let i=0;
+
+  // do {
+    
+  //   let item = inputList[i];
+      
+  //   let temp = `<ul id="ul_${i}">`
+  //   temp    += `  <li>${i}</li>`
+  //   temp   += `  <li><a href="./detail.html?id=${item.id}">${item.title}</a></li>`
+  //   temp   += `  <li>${item.reg}</li>`
+  //   temp   += `  <li>${item.reg_date}</li>`
+  //   temp    += `</ul>`
+  //   temp    += `<hr />`
+
+  //   list.innerHTML += temp;
+
+  //   i++;
+
+  // } while ( inputList.length >= i )
 }
 
