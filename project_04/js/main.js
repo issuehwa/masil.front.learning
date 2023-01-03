1e8aa7c0a27b02b261f412f42194c40c78cdd0a2
SYililiii <witiss0@daum.net>
2023-01-03 17:51:57 +0900

    230103 - 메인화면 출력까지

--- project_04/js/main.js
+++ project_04/js/main.js
@@ -1,21 +1,46 @@
 //주소 파라미터값을 가져옵니다.
 //사용법 => let params = ''.getParam('?');
 
-
-window.onload = () => {
-  reanderNavList();
-};
+// window.onload = () => {
+//   reanderNavList();
+// };
 
 const nav = document.getElementById("nav_side");
+const main = document.getElementById("main");
 
 function reanderNavList() {
-  for (let i = 0; dataInfo.categoryList.length >= i; i++) {
+  for (let i = 0; dataInfo.categoryList.length > i; i++) {
     let list = dataInfo.categoryList[i];
-    let temp = `<a href="./index.html?id=${list.step}"><li>${list.step}</li></a>`;
+    let temp = `<a href="./index.html?id=${list.sort}"><li>${list.step}</li></a>`;
     nav.innerHTML += temp;
   }
 }
 
+reanderNavList();
 
+const urlParams = new URL(location.href).searchParams;
+let sortId = urlParams.get("id");
+console.log(sortId);
+console.log(dataInfo.list[9].sort);
+console.log(dataInfo.list[7].detailList[0].step);
 
-// console.log('params', params, params.id)
+renderMainList();
+
+function renderMainList() {
+  let i = 0;
+  while (i < dataInfo.categoryList.length) {
+    if (sortId == dataInfo.list[i].sort) {
+      let ctList = dataInfo.categoryList[i];
+      let list = dataInfo.list[i];
+
+      let temp = `<b>${ctList.step}</b>`;
+      temp += `<hr />`;
+      for (count = 0; count < dataInfo.categoryList.length; count++) {
+        temp += `<ol><li>${list.detailList[count].step}</li><div></div><li>${list.detailList[count].title}</li></ol>`;
+        temp += `<hr /> `;
+      }
+      main.innerHTML += temp;
+    }
+    i++;
+  }
+}
